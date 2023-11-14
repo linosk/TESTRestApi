cef0e5d7d62dcbf9734f6623160088da505a1249
linosk <linakoskar@gmail.com>
2023-11-14 19:38:28 +0100

    First test corrected

--- test/test_main.py
+++ test/test_main.py
@@ -1,25 +1,24 @@
 import unittest
-from psycopg2 import Error, OperationalError
-from psycopg2.extras import RealDictCursor
 import app.main
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-#find out the naming convention
-#think about better structuring
-#is this good practice?
 class TestMain(unittest.TestCase):
 
-    @unittest.expectedFailure
     def test_get_database_cursor(self):
 
         dbcred = {
             "user": "postgres",
-            "password": "wrong_password",
+            "password": "postgres",
             "host": "127.0.0.1",
             "port": "5432",
             "database": "fastapi",
             "cursor_factory": RealDictCursor
         }
 
-        with self.assertRaises(OperationalError):
-            cursor = app.main.get_database_cursor(dbcred)
+        cursor = app.main.get_database_cursor(dbcred)
 
+        self.assertIsInstance(cursor, psycopg2.extensions.cursor)
+
+        cursor.close()
+        cursor.connection.close()
