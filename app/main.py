24d0bbccbaf2264617f3e77d4aef4bf34eaec6b7
linosk <linakoskar@gmail.com>
2023-11-16 19:23:10 +0100

    Extracting code and spliting it into seperate modules

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
-import psycopg2
-from psycopg2 import Error
-from psycopg2.extras import RealDictCursor
-from fastapi import FastAPI
+import database_connect
 
 def main():
     
@@ -16,47 +13,8 @@ def main():
 
     cursor = get_database_cursor(dbcred)
 
-    api = FastAPI
-
-    @api.get("/")
-    def read_root():
-        return {"Hello": "World"}
-
-    @api.post("/")
-    @api.delete("/")
-    @api.put("/")
-
     cursor.close()
     cursor.connection.close()
-
-def get_database_cursor(credentials):
-    
-    try:
-        connection = psycopg2.connect(
-            user=credentials["user"],
-            password=credentials["password"],
-            host=credentials["host"],
-            port=credentials["port"],
-            database=credentials["database"],
-            cursor_factory=credentials["cursor_factory"]
-        )
-
-        cursor = connection.cursor()
-        print("Database connection was succesfull.")
-
-        #cursor.execute("SELECT * FROM games;")
-        #result = cursor.fetchall()
-        #print(result)
-
-        return cursor
-
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-
-    finally:
-        if connection is not None:
-            connection.close()
-            print("Database connection closed.")
 
 if __name__ == '__main__':
     main()
